4d761902f135248b31ba5e9812fde22820538e1f
AvetisDN <avetisdn@gmail.com>
2022-10-16 09:44:02 +0300

    initial

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-100 p-10 rounded-xl shadow-xl">
+      <h1 className="text-slate-600 text-3xl font-black">
+        NextJS + Tailwindcss
+      </h1>
+    </div>
+  );
+}
